73e7cba91f0d1ffb7ba17a159d8da33a026982be
Charles Bryan <cfb3@uw.edu>
2020-04-26 18:54:58 -0700

    pushme: change send on err in login to json

--- routes/login.js
+++ routes/login.js
@@ -52,7 +52,7 @@ router.get('/', (request, response) => {
         pool.query(theQuery, values)
             .then(result => { 
                 if (result.rowCount == 0) {
-                    response.status(404).send({
+                    response.status(404).json({
                         message: 'User not found' 
                     })
                     return
